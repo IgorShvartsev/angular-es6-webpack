6ce7b5138e4a4dcee4149e84fa866a5b7663ad82
shvartsev <igor.shvartsev@gmail.com>
2016-11-12 11:47:58 +0200

    Update HomeCtrl.js

--- src/modules/home/controller/HomeCtrl.js
+++ src/modules/home/controller/HomeCtrl.js
@@ -4,6 +4,6 @@ export class HomeCtrl {
 		"ngInject";
 		this.$location = $location;
 		this.headline = 'You are on the Home page';
-    	this.intro = "Lorem ipsum dolor sit amet, has quando denique prodesset ad, pro erat dissentias eu. In per definiebas interpretaris, et sea veritus oporteat, qui no mucius theophrastus. Ne est tation exerci, pri possim deleniti mediocritatem ut. Eum ne semper mediocrem scripserit. Ei per minim homero appetere, no nam salutatus consulatu. Quodsi suscipit petentium ut pri, malis ornatus vituperata pro at.";
+		this.intro = "Lorem ipsum dolor sit amet, has quando denique prodesset ad, pro erat dissentias eu. In per definiebas interpretaris, et sea veritus oporteat, qui no mucius theophrastus. Ne est tation exerci, pri possim deleniti mediocritatem ut. Eum ne semper mediocrem scripserit. Ei per minim homero appetere, no nam salutatus consulatu. Quodsi suscipit petentium ut pri, malis ornatus vituperata pro at.";
 	}
 };
